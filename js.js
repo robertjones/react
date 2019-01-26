acfe3bec10358b69ce23c5af78c10061ccb5e697
Rob Jones <robertjones@users.noreply.github.com>
2019-01-26 22:53:33 +0000

    2 player

--- js.js
+++ js.js
@@ -1,13 +1,3 @@
-// function Welcome(props) {
-//   return <h1>Hello, {props.name}</h1>;
-// }
-
-// const element = <Welcome name="Sara" />;
-// ReactDOM.render(
-//   element,
-//   document.getElementById('root')
-// );
-
 'use strict';
 
 const e = React.createElement;
@@ -31,20 +21,56 @@ class LikeButton extends React.Component {
   }
 }
 
-var data = [1, null, null,
-        null, 2, null,
-        null, null, 3,]
+var Data = Array(9).fill('_');
+var isXTurn = true;
+
+function isWinner(data, player) {
+  const moves = [[0,1,2],
+           [3,4,5],
+           [6,7,8],
+           [0,3,6],
+           [1,4,7],
+           [2,5,8],
+           [0,4,8],
+           [2,4,6]];
+  return moves
+    .map(cells => 
+      cells.every(
+        x => data[x]==player))
+    .some(x => x)
+};
+        
+function clicky(id) {
+  if (Data[id] != '_' || isWinner(Data, 'X') || isWinner(Data, 'O')) { return 0 };
+  const player = isXTurn ? 'X' : 'O';
+  Data[id] = isXTurn ? 'X' : 'O';
+  const msg = isWinner(Data, player) ? player + ' wins!' : ''
+  ReactDOM.render(
+    e(()=>Board({msg: msg})), 
+    domContainer);
+  isXTurn = !isXTurn;
+};
+
+
 
 function Square(props) {
-  return e('button', null, props.say)
+  return e('button', 
+           {style: {width: '100px', height: '100px'}, 
+           onClick: () => clicky(props.id)}, 
+           Data[props.id])
 };
 
 function Board(props) {
   return e('div', null,
     ...[0,1,2].map(i => e('div', {id: 'row'}, 
-      ...[0,1,2].map(j => e(Square, {id: data[j+i*3], say: 'hi'})))))
+      ...[0,1,2].map(j => e(Square, 
+        {id: j+i*3})))),
+        MsgBox(props.msg))
 };
 
+function MsgBox(msg) {
+  return e('div', null, msg)
+};
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(Board, data), domContainer);
+const domContainer = document.querySelector('#app');
+ReactDOM.render(e(Board), domContainer);
